5f413206252227fab0cced0942dce96f38e5db8a
Matthew Zhou <emzeegotanemail2@gmail.com>
2024-05-10 18:30:46 -0400

    Implement 256 colors

--- lua/flutter-tools/ansi_colorizer.lua
+++ lua/flutter-tools/ansi_colorizer.lua
@@ -1,11 +1,11 @@
 local function dump(o)
-  if type(o) == 'table' then
-    local s = '{ '
+  if type(o) == "table" then
+    local s = "{ "
     for k, v in pairs(o) do
-      if type(k) ~= 'number' then k = '"' .. k .. '"' end
-      s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+      if type(k) ~= "number" then k = '"' .. k .. '"' end
+      s = s .. "[" .. k .. "] = " .. dump(v) .. ","
     end
-    return s .. '} '
+    return s .. "} "
   else
     return tostring(o)
   end
@@ -32,25 +32,90 @@ local base16_colors = {
   { color = nil,       bg_id = 49 },              -- Default bg
 }
 
--- Retrieves color information based on the provided ID.
--- If the ID is 0, returns "clear" indicating no color.
--- If the ID matches a foreground or background ID in the base16_colors table,
--- returns a tuple containing either "fg" or "bg" along with the corresponding color value.
--- If no match is found, returns nil.
-local function get_colors_by_id(id)
-  if id == 0 then
-    return "clear"
-  end
+local base256_colors = {}
 
-  for _, base16_color in ipairs(base16_colors) do
-    if id == base16_color.fg_id then
-      return "fg", base16_color.color
-    elseif id == base16_color.bg_id then
-      return "bg", base16_color.color
+-- Original 16 colors
+for i, value in ipairs(base16_colors) do
+  base256_colors[i - 1] = value.color
+end
+-- 216 colors after original 16 colors
+for i = 16, 231, 1 do
+  local i_offset = i - 16
+
+  local RGB = {
+    red = math.floor(i_offset / 36),
+    green = math.floor((i_offset % 36) / 6),
+    blue = i_offset % 6,
+  }
+
+  for color, value in pairs(RGB) do
+    if value ~= 0 then
+      RGB[color] = value * 40 + 55
     end
   end
 
-  return nil
+  base256_colors[i] = string.format("#%02x%02x%02x", RGB["red"], RGB["green"], RGB["blue"])
+end
+-- Grey scale colors
+for i = 232, 255, 1 do
+  local lightness = 10 * (i - 232) + 8
+  base256_colors[i] = string.format("#%02x%02x%02x", lightness, lightness, lightness)
+end
+
+---Gets the color information from an ANSI color code
+---@param color_code string The ANSI color code
+-- nil represents reset, -1 represents no-op
+local function get_colors_by_escape(color_code)
+  local READING_STATE = {
+    default = 0,    -- Default state
+    pending_fg = 1, -- Deciding if should read a 256 Color or RGB value for the foreground
+    pending_bg = 2, -- Deciding if should read a 256 Color or RGB value for the background
+    fg_256 = 3,     -- Ready to read a 256 foreground color
+    bg_256 = 4,     -- Ready to read a 256 background color
+  }
+  local fg, bg = -1, -1
+  local current_state = READING_STATE.default ---@type integer
+
+  -- Parse all the colors in the escape sequence
+  for color_id in string.gmatch(color_code, "([^;]+)") do
+    if current_state == READING_STATE.pending_fg then
+      if color_id == "5" then
+        current_state = READING_STATE.fg_256
+      else
+        current_state = READING_STATE.default
+      end
+    elseif current_state == READING_STATE.pending_bg then
+      if color_id == "5" then
+        current_state = READING_STATE.bg_256
+      else
+        current_state = READING_STATE.default
+      end
+    elseif current_state == READING_STATE.fg_256 then
+      fg = base256_colors[tonumber(color_id)]
+      current_state = READING_STATE.default
+    elseif current_state == READING_STATE.bg_256 then
+      bg = base256_colors[tonumber(color_id)]
+      current_state = READING_STATE.default
+    elseif current_state == READING_STATE.default then
+      if color_id == "0" then
+        fg, bg = nil, nil
+      elseif color_id == "38" then
+        current_state = READING_STATE.pending_fg
+      elseif color_id == "48" then
+        current_state = READING_STATE.pending_bg
+      else
+        for _, base16_color in ipairs(base16_colors) do
+          if color_id == tostring(base16_color.fg_id) then
+            fg = base16_color.color
+          elseif color_id == tostring(base16_color.bg_id) then
+            bg = base16_color.color
+          end
+        end
+      end
+    end
+  end
+
+  return { fg = fg, bg = bg }
 end
 
 local function parse_ansi_colors(line, prev_fg, prev_bg)
@@ -58,7 +123,7 @@ local function parse_ansi_colors(line, prev_fg, prev_bg)
   local remaining_line = line
 
   while true do
-    local escape_start, escape_end, color_code = string.find(remaining_line, '\27%[([%d;]+)m')
+    local escape_start, escape_end, color_code = string.find(remaining_line, "\27%[([%d;]+)m")
 
     -- Check if escape code was not found
     if not escape_start then
@@ -67,7 +132,7 @@ local function parse_ansi_colors(line, prev_fg, prev_bg)
     end
 
     local text_before_escape = string.sub(remaining_line, 1, escape_start - 1)
-    if text_before_escape ~= '' then
+    if text_before_escape ~= "" then
       if prev_fg == nil and prev_bg == nil then
         table.insert(parsed_line, text_before_escape)
       else
@@ -77,15 +142,12 @@ local function parse_ansi_colors(line, prev_fg, prev_bg)
     end
 
     -- Parse all the colors in the escape sequence
-    for color_id in string.gmatch(color_code, "([^;]+)") do
-      local type, color = get_colors_by_id(tonumber(color_id))
-      if type == 'clear' then
-        prev_fg, prev_bg = nil, nil
-      elseif type == 'fg' then
-        prev_fg = color
-      elseif type == 'bg' then
-        prev_bg = color
-      end
+    local result = get_colors_by_escape(color_code)
+    if result.fg ~= -1 then
+      prev_fg = result.fg
+    end
+    if result.bg ~= -1 then
+      prev_bg = result.bg
     end
 
     if prev_fg == nil and prev_bg == nil then
@@ -93,7 +155,7 @@ local function parse_ansi_colors(line, prev_fg, prev_bg)
       remaining_line = string.sub(remaining_line, escape_end + 1)
     else
       local text_start = escape_end + 1
-      local text_end = string.find(remaining_line, '\27%[', text_start) or (#remaining_line + 1)
+      local text_end = string.find(remaining_line, "\27%[", text_start) or (#remaining_line + 1)
       local text = string.sub(remaining_line, text_start, text_end - 1)
 
       table.insert(parsed_line,
@@ -122,12 +184,13 @@ local function highlight_ansi_colors(bufnr)
   for i, parsed_line in ipairs(parsed_lines) do
     local col = 0
     for _, part in ipairs(parsed_line) do
-      if type(part) == 'table' then
+      if type(part) == "table" then
         local text = part.text
         local fg_color = part.fg_color or ""
         local bg_color = part.bg_color or ""
         local escape_length = part.escape_length or 0
-        local hl_group = 'AnsiColor' .. 'fg' .. fg_color:gsub("#", "") .. 'bg' .. bg_color:gsub("#", "")
+
+        local hl_group = "AnsiColor" .. "fg" .. fg_color:gsub("#", "") .. "bg" .. bg_color:gsub("#", "")
 
         col = col + escape_length
         vim.api.nvim_set_hl(0, hl_group, { fg = fg_color, bg = bg_color })
@@ -150,6 +213,10 @@ end
 function M.conceal_ansi_color_codes()
   vim.cmd([[set conceallevel=2]])
   vim.cmd([[syntax match ConcealANSI /[^m]*m/ conceal]])
+end
+
+function M.print_256()
+  print(dump(base256_colors))
 end
 
 return M
